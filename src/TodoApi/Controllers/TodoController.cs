79a8fddcd78378b5ab1edf6ac3d2a0eb0b4f0b9b
Hameed Goudat <goudat@ymail.com>
2016-07-10 12:41:37 -0500

    Added TodoController with CRUD Operations.

---
+++ src/TodoApi/Controllers/TodoController.cs
@@ -0,0 +1,75 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using TodoApi.Models;
+
+namespace TodoApi.Controllers
+{
+    [Route("api/[controller]")]
+    public class TodoController : Controller
+    {
+        public TodoController(ITodoRepository todoItems)
+        {
+            TodoItems = todoItems;
+        }
+        public ITodoRepository TodoItems { get; set; }
+
+        // Get All Todos
+        public IEnumerable<TodoItem> GetAll()
+        {
+            return TodoItems.GetAll();
+        }
+
+        // Get Todos by ID
+        [HttpGet("{id}", Name = "GetTodo")]
+        public IActionResult GetById(string id)
+        {
+            var item = TodoItems.Find(id);
+            if (item == null)
+            {
+                return NotFound();
+            }
+            return new ObjectResult(item);
+        }
+
+        // Add New Todo
+        [HttpPost]
+        public IActionResult Create([FromBody] TodoItem item)
+        {
+            if (item == null)
+            {
+                return BadRequest();
+            }
+            TodoItems.Add(item);
+            return CreatedAtAction("GetTodo", new { id = item.Key }, item);
+        }
+
+        // Update Todo by ID
+        [HttpPut("{id}")]
+        public IActionResult Update(string id, [FromBody] TodoItem item)
+        {
+            if (item == null || item.Key != id)
+            {
+                return BadRequest();
+            }
+
+            var todo = TodoItems.Find(id);
+            if (todo == null)
+            {
+                return NotFound();
+            }
+
+            TodoItems.Update(item);
+            return new NoContentResult();
+        }
+
+        // Delete Todo by ID
+        [HttpDelete("{id}")]
+        public void Delete(string id)
+        {
+            TodoItems.Remove(id);
+        }
+    }
+}
